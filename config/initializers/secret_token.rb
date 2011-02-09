172066384ee7010f539a7f52ad57ddbb00288cc0
Chris Saylor <cwsaylor@gmail.com>
2011-02-09 15:48:17 -0800

    Updating to 3.0.4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Memory::Application.config.secret_token = '8768ef76b5b321e7412a43e4893037bb9610fd6a383e9532c9d1f5385df079db6a4a6863877d0424dae6ee64067ab20b29c561fa79233b5170f5dcce0e984367'
+Memory::Application.config.secret_token = '41f2dbd86f59336c6107b6108b90790f141b4831e6f624fd3d4dee2beda0bb2f9b5babf480bafd7fff04026b9e1ed7c26d0d62040ed8365239cd125effea428c'
