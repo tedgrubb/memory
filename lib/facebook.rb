d7de794f1ae2e4236e8b01d7f55cdeec26decdb8
Chris Saylor <cwsaylor@gmail.com>
2011-02-15 09:23:09 -0800

    Adding a post method to the facebook lib.

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -7,6 +7,10 @@ class Facebook
     self.class.default_params :access_token => token
   end
 
+  def post(path, query = {})
+    self.class.post(path, :query => query)
+  end
+
   # HTTParty get is on the class, so let's call that one and wrap it up in some error handling.
   def get(path, query = {})
     begin
